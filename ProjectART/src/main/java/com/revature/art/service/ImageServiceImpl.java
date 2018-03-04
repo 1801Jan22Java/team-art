0610c53b3e78b3dc9017fe807c749b763a94c3f1
ginyandrew <ginyandrew@gmail.com>
2018-03-03 20:35:46 -0500

    Add @Service annotation in ImageServiceImpl.java

--- ProjectART/src/main/java/com/revature/art/service/ImageServiceImpl.java
+++ ProjectART/src/main/java/com/revature/art/service/ImageServiceImpl.java
@@ -3,6 +3,7 @@ package com.revature.art.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.art.dao.AnimalDao;
 import com.revature.art.dao.ApplicationDao;
@@ -10,6 +11,7 @@ import com.revature.art.dao.FileDao;
 import com.revature.art.dao.MeetupDao;
 import com.revature.art.dao.UserDao;
 
+@Service
 public class ImageServiceImpl implements ImageService {
 
 private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
