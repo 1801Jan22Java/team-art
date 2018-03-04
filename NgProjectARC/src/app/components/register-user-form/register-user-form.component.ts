e5ac0e0575563879c77ad86e73ea6448cd6f1743
ginyandrew <ginyandrew@gmail.com>
2018-03-04 00:47:03 -0500

    share value in multiple angular components

--- NgProjectARC/src/app/components/register-user-form/register-user-form.component.ts
+++ NgProjectARC/src/app/components/register-user-form/register-user-form.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NgForm, FormGroup, FormControl, Validators} from '@angular/forms';
 import {Router} from "@angular/router";
 
-// service 
+// Gin's!
 import {UserService} from '../../service/user.service';
 
 @Component({
