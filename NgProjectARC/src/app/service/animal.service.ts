b1f1bdfb890e3aebef9789b283a3fa51273ed9b8
Imperfect1987 <JamesWRules@gmail.com>
2018-03-04 01:28:32 -0500

    worth a try

--- NgProjectARC/src/app/service/animal.service.ts
+++ NgProjectARC/src/app/service/animal.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { AnimalList } from '../components/managelist-of-animals/managelist-of-animals.component';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class AnimalService {
 
-  constructor() { }
 
   //Evan
   
   
   // James
-  
+  private _url: string = "http://localhost:8080/api/animal/animalList.json";
+  constructor(private http: HttpClient) { }
+
+  getApplication(): Observable<AnimalList[]>{
+    //console.log(this.http.get(this._url));
+    //console.log(this.http.get<AnimalList[]>(this._url));
+    return this.http.get<AnimalList[]>(this._url);
+  }
   
   // Eric
   
