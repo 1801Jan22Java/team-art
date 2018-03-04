3ecfd8e123bc027a71356f7aa233e520a62ed8f8
ginyandrew <ginyandrew@gmail.com>
2018-03-04 10:24:29 -0500

    add comment in ApplicationService

--- ProjectART/src/main/java/com/revature/art/service/ApplicationService.java
+++ ProjectART/src/main/java/com/revature/art/service/ApplicationService.java
@@ -7,11 +7,15 @@ import com.revature.art.domain.Application;
 public interface ApplicationService {
 
 	// Eric
-
+	//written by Gin but was commented out when I pulled
+	//please don't move any code written by others.. if want to use, just call in your controller..
+	List<Application> getAdpAplcList();	
+	
+	Application updateApplication(Application a);	
 	// Evan
 
 	// James
 
 	// Gin
- 
+	 
 }
