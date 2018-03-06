699f0369c94eaedba18ad52f27d626443c05dc6d
Imperfect1987 <JamesWRules@gmail.com>
2018-03-05 21:58:16 -0500

    register animal now works praise me please

--- NgProjectARC/src/app/components/register-animal-form/register-animal-form.component.ts
+++ NgProjectARC/src/app/components/register-animal-form/register-animal-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AnimalList } from '../managelist-of-animals/managelist-of-animals.component';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 //James'!!!!!!! 
 @Component({
   selector: 'app-register-animal-form',
@@ -6,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-animal-form.component.css']
 })
 export class RegisterAnimalFormComponent implements OnInit {
+  
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  aniID: number= 0;
+  adoptstat: string = "";
+  gend: string = "";
+  mat: string = "";
+  pname: string = "";
+  spec: string = "";
+  animal: AnimalList;
+  //animal: Observable<AnimalList>;
+  private _url: string = "http://localhost:8080/api/animal/addAnimal";
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+
+    this.rForm = fb.group({
+      'pname' : [null, Validators.required],
+      'mat' : [Validators.required],
+      'spec' : [Validators.required],
+      'gend' : [Validators.required]     
+    });
+   }
+
+  
 
   ngOnInit() {
   }
+
+  addPost(post) {
+    this.pname = post.pname;
+    this.mat = post.mat;
+    this.spec = post.spec;
+    this.gend = post.gend;
+    console.log(this.pname);
+    console.log(this.mat);
+    console.log(this.spec);
+    console.log(this.gend);
+    this.animal = {
+      animalID: null,
+      name : this.pname,
+      maturity : this.mat,
+      gender : this.gend,
+      adoptStatus : "Available",
+      species : this.spec 
+    };
+
+    console.log(this.animal);
+    let httpSend = this.http.post(this._url, this.animal).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+       console.log("Error occurred");
+        console.log(err);
+      }
+    );
+    console.log(httpSend);
+
+    }
+    
+  
 
 }
