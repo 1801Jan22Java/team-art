bf4f9d209a8d31c0a79cd02810f096094be280d3
Imperfect1987 <JamesWRules@gmail.com>
2018-03-07 18:04:06 -0500

    this is not important

--- ProjectART/src/main/java/com/revature/art/controller/ImageController.java
+++ ProjectART/src/main/java/com/revature/art/controller/ImageController.java
@@ -1,5 +1,9 @@
 package com.revature.art.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 
 import javax.servlet.annotation.MultipartConfig;
@@ -9,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,10 +22,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 import com.revature.art.domain.Animal;
-import com.revature.art.domain.File;
+//import com.revature.art.domain.File;
 import com.revature.art.service.AnimalService;
 import com.revature.art.service.ApplicationService;
 import com.revature.art.service.ImageService;
@@ -50,16 +58,35 @@ public class ImageController {
 	// Evan
 	
 	// James
-	@PostMapping("/addImage")
-	public @ResponseBody int addFile(@RequestBody File file){
+	@PostMapping("/mapToAnimal")
+	public @ResponseBody int addFile(@RequestBody com.revature.art.domain.File file){
 		int imageID =imageService.addFile(file);
 		logger.info(file.toString());
+		System.out.println(imageID);
 		return imageID;
 	}
 	//, headers = "'Content-Type': 'multipart/form-data'"
 	//@RequestParam (value="file") MultipartFile file
 	@PostMapping(value = "/physicalImage")
-	public @ResponseBody String physicalFile(@RequestParam (value="file") MultipartFile file) {
+	public @ResponseBody String physicalFile(@RequestParam (value="file") MultipartFile file, Model model) throws IOException {
+		String directory = "C:\\ART Project\\team-art\\ProjectART\\src\\main\\webapp\\WEB-INF\\images";
+		int maxSize = 1024 * 1024 * 20;
+		String encoding = "UTF-8";
+		//System.out.println(aniID);
+		//MultipartRequest multiRequest = new MultipartRequest(file, directory, maxSize, encoding);
+		if (!file.getOriginalFilename().isEmpty()) {
+            BufferedOutputStream outputStream = new BufferedOutputStream(
+                  new FileOutputStream(
+                        new File(directory, file.getOriginalFilename())));
+
+            outputStream.write(file.getBytes());
+            outputStream.flush();
+            outputStream.close();
+         } else {
+            model.addAttribute("msg", "Please select at least one file..");
+            return "fileUploadForm";
+         }
+   
 		System.out.println("wootwoot");
 		return "";
 	}
