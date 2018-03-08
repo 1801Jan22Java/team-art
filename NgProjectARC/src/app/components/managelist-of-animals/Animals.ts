841e5e9d52754b414e1ddd0bf6e106c4cde27ee8
Imperfect1987 <JamesWRules@gmail.com>
2018-03-08 02:28:54 -0500

    money in the bank

--- NgProjectARC/src/app/components/managelist-of-animals/Animals.ts
+++
@@ -1,8 +0,0 @@
-export class Animal {
-    animalId: number;
-    adoptStatus: string;
-    gender: string;
-    maturity: string;
-    name: string;
-    species: string;
-}
--- NgProjectARC/src/app/models/file.ts
+++
@@ -1,8 +0,0 @@
-import { AnimalList } from '../components/managelist-of-animals/managelist-of-animals.component';
-
-export class File {
-    fileID: number;
-    filename: string;
-    animal: AnimalList;
-
-}
