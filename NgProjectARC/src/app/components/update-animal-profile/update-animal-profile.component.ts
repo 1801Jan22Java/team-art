79f27ccb40c94f35be12a165c1a5e0de99307b9a
CarpizoeE <you@example.com>
2018-03-07 23:42:57 -0500

    Update animal feature

--- NgProjectARC/src/app/components/update-animal-profile/update-animal-profile.component.ts
+++ NgProjectARC/src/app/components/update-animal-profile/update-animal-profile.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Router} from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {AnimalList} from '../managelist-of-animals/managelist-of-animals.component';
-import {ManagelistOfAnimalsComponent } from '../managelist-of-animals/managelist-of-animals.component'
-//James'!!!!!!! 
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AnimalList } from "../managelist-of-animals/managelist-of-animals.component";
+import { ManagelistOfAnimalsComponent } from "../managelist-of-animals/managelist-of-animals.component";
+import { AnimalService } from "../../service/animal.service";
+import { Animal } from "../../models/animal";
+
+//James'!!!!!!!
 @Component({
-  selector: 'app-update-animal-profile',
-  templateUrl: './update-animal-profile.component.html',
-  styleUrls: ['./update-animal-profile.component.css']
+  selector: "app-update-animal-profile",
+  templateUrl: "./update-animal-profile.component.html",
+  styleUrls: ["./update-animal-profile.component.css"]
 })
 export class UpdateAnimalProfileComponent implements OnInit {
-
   rForm: FormGroup;
   iForm: FormGroup;
-  post: any;                     // A property for our submitted form
+  post: any; // A property for our submitted form
   iPost: any;
-  aniID: number= 0;
+  aniID: number = 0;
   adoptstat: string = "";
   gend: string = "";
   mat: string = "";
@@ -30,32 +32,49 @@ export class UpdateAnimalProfileComponent implements OnInit {
   image4: string = "";
   animal: AnimalList;
   headers: HttpHeaders;
-  
-  @ViewChild('image1') image: ElementRef;
+
+  // Eric Begin
+  toggle;
+  currentAnimal: Animal;
+  gender: string;
+  maturity: string;
+  species: string;
+  // Eric End
+
+  @ViewChild("image1") image: ElementRef;
   //animal: Observable<AnimalList>;
   private _url: string = "http://localhost:8080/api/animal/updateAnimal";
   private url_: string = "http://localhost:8080/api/image/physicalImage";
 
-
-  constructor(private fb: FormBuilder, private http: HttpClient,
-  private router: Router) {
-
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private animalList: ManagelistOfAnimalsComponent,
+    private animalService: AnimalService
+  ) {
     this.rForm = fb.group({
-      'pname' : [null, Validators.required],
-      'mat' : [Validators.required],
-      'spec' : [Validators.required],
-      'gend' : [Validators.required],
+      pname: [null, Validators.required],
+      mat: [Validators.required],
+      spec: [Validators.required],
+      gend: [Validators.required]
     });
 
     this.iForm = fb.group({
-      'image1' : [null, Validators.required],
-      'image2' : [null],
-      'image3' : [null],
-      'image4' : [null]
+      image1: [null, Validators.required],
+      image2: [null],
+      image3: [null],
+      image4: [null]
     });
-   }
+  }
   ngOnInit() {
-    console.log(this.image);
+    //console.log(this.image);
+    this.animalService
+      .getAnimalById(this.animalList.currentAnimal.animalID)
+      .subscribe(data => {
+        this.currentAnimal = data;
+      });
+    this.toggle = true;
   }
 
   addPost(post) {
@@ -63,74 +82,91 @@ export class UpdateAnimalProfileComponent implements OnInit {
     this.mat = post.mat;
     this.spec = post.spec;
     this.gend = post.gend;
-    console.log(this.pname);
-    console.log(this.mat);
-    console.log(this.spec);
-    console.log(this.gend);
+    ///console.log(this.pname);
+    //console.log(this.mat);
+    //console.log(this.spec);
+    //console.log(this.gend);
     this.animal = {
-      animalID: 63,
-      name : this.pname,
-      maturity : this.mat,
-      gender : this.gend,
-      adoptStatus : "Available",
-      species : this.spec 
+      animalID: this.currentAnimal.animalID,
+      name: this.pname,
+      maturity: this.mat,
+      gender: this.gend,
+      adoptStatus: "Available",
+      species: this.spec
     };
 
-    console.log(this.animal);
-    let httpSend = this.http.post(this._url, this.animal).subscribe(
+    this.currentAnimal.gender = this.gender;
+    this.currentAnimal.maturity = this.maturity;
+    this.currentAnimal.species = this.species;
+    //console.log(this.animal);
+    let httpSend = this.http.post(this._url, this.currentAnimal).subscribe(
       res => {
         console.log(res);
       },
       err => {
-       console.log("Error occurred");
+        console.log("Error occurred");
         console.log(err);
       }
     );
-    console.log(httpSend);
+    //console.log(httpSend);
+    this.hide();
+  }
 
-    }
+  Cancel() {
+    //console.log("yay")
+    this.router.navigate(["/animalList"]);
+  }
 
-    Cancel(){
-      console.log("yay")
-      this.router.navigate(['/animalList']);
-    }
-
-  
-    addImages(iPost){
-      this.image1 = iPost.image1;
-      this.image2 = iPost.image2;
-      this.image3 = iPost.image3;
-      this.image4 = iPost.image4;
-      let fileBrowser = this.image.nativeElement;
-      console.log(fileBrowser.files[0]);
+  addImages(iPost) {
+    this.image1 = iPost.image1;
+    this.image2 = iPost.image2;
+    this.image3 = iPost.image3;
+    this.image4 = iPost.image4;
+    let fileBrowser = this.image.nativeElement;
+    console.log(fileBrowser.files[0]);
     if (fileBrowser.files && fileBrowser.files[0]) {
       const formData = new FormData();
       formData.append("file", fileBrowser.files[0]);
-      
+
       //formData.append("file"), fileBrowser.
       /*this.headers = new HttpHeaders({
         'Content-Type': 'multipart/form-data'
        });*/
-       //'multipart/form-data'
+      //'multipart/form-data'
       /*
      let fileList: FileList = iPost.target.files;
       if(fileList.length > 0) {
           let file: File = fileList[0];
           let formData:FormData = new FormData();
           formData.append('uploadFile', file, file.name); */
-          //{headers: this.headers}
-      let httpSend = this.http.post(this.url_, formData ).subscribe(
+      //{headers: this.headers}
+      let httpSend = this.http.post(this.url_, formData).subscribe(
         res => {
           console.log(res);
         },
         err => {
-         console.log("Error occurred");
+          console.log("Error occurred");
           console.log(err);
         }
       );
       console.log("cool");
     }
     console.log(fileBrowser.files[0]);
+    this.hide();
   }
 
+  // Eric Begin
+  hide() {
+    this.animalList.manageForm = false;
+    this.toggle = true;
+  }
+
+  updateName(event: any) {
+    if (event !== "") {
+      this.currentAnimal.name = event.target.value;
+      console.log(this.currentAnimal.name);
+    }
+  }
+
+  //Eric End
 }
--- NgProjectARC/src/app/service/animal.service.ts
+++ NgProjectARC/src/app/service/animal.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AnimalList } from '../components/managelist-of-animals/managelist-of-animals.component';
 import { Observable } from 'rxjs/Observable';
+import { Animal } from '../models/animal';
 @Injectable()
 export class AnimalService {
 
@@ -20,7 +21,16 @@ export class AnimalService {
   }
   
   // Eric
-  
+  private ROOT_URL: string = "http://localhost:8080/api/animal";
+  getAnimalById(animalID: number): Observable<Animal> {
+    return this.http.post<Animal>(this.ROOT_URL + '/getAnimalById', animalID);
+  }
+
+  getAnimals(): Observable<Animal[]>{
+    //console.log(this.http.get(this._url));
+    //console.log(this.http.get<AnimalList[]>(this._url));
+    return this.http.get<Animal[]>(this.ROOT_URL + '/animalList.json');
+  }
   
   // Gin
   
